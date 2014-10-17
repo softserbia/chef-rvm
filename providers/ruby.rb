ac675f86b3f2288d304646677dd2d69f0ac6c723
softserbia <tech@softserbia.com>
2014-10-17 14:31:25 +0200

    Attempt to support debian 5 lenny

--- providers/ruby.rb
+++ providers/ruby.rb
@@ -129,10 +129,15 @@ def install_ruby_dependencies(rubie)
   when /^ruby-/, /^ree-/, /^rbx-/, /^kiji/
     case node['platform']
       when "debian","ubuntu"
-        pkgs  = %w{ build-essential openssl libreadline6 libreadline6-dev
+        pkgs  = %w{ build-essential openssl 
                     zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev
                     sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev
                     ncurses-dev automake libtool bison ssl-cert pkg-config libgdbm-dev libffi-dev}
+        if node['platform_version'] < 6.0
+          pkgs += %w{ libreadline5 libreadline5-dev }
+        else
+          pkgs += %w{ libreadline6 libreadline6-dev }
+        end
         pkgs += %w{ subversion }  if rubie =~ /^ruby-head$/
       when "suse"
         pkgs = %w{ gcc-c++ patch zlib zlib-devel libffi-devel
